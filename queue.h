30faf0018aca2c451998e8ed9dbb1f7d3caab815
Jude <judemdonahue@gmail.com>
2025-11-13 12:04:00 -0600

    ifndef define and endif for QUEUE_H

---
+++ queue.h
@@ -0,0 +1,10 @@
+/* ******************************************
+ * File:   queue.h
+ * Author: Jude Donahue
+ * 
+ * ******************************************/
+
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#endif /* QUEUE_H */
