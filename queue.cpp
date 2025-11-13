03b87411b09918854b371bf7f184e7062322f580
Jude <judemdonahue@gmail.com>
2025-11-13 12:34:31 -0600

    added Constructor and Destructor

--- queue.cpp
+++ queue.cpp
@@ -3,3 +3,14 @@
  * Author: Jude Donahue
  * 
  * ******************************************/
+#include "queue.h"
+
+Queue::Queue(bool fifo) {
+    isFifo = fifo;
+    head = NULL;
+    tail = NULL;
+}
+
+Queue::~Queue() {
+    
+}
